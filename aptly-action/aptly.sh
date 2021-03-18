95cd717c3abb9a1e9ab44c4d88e7b8e817df530e
andre <andre.horn@erweka.com>
2021-03-18 11:26:57 +0100

    NG-1032: create folder

--- aptly-action/aptly.sh
+++ aptly-action/aptly.sh
@@ -6,13 +6,13 @@ then
   aptly mirror update -ignore-signatures erweka-mirror
   aptly snapshot create erweka-snapshot from mirror erweka-mirror
 
-  aptly repo create -distribution=bionic -component=main erweka-"${{ inputs.repo-name }}"
-  aptly repo add erweka-"${{ inputs.repo-name }}" deb-package
-  aptly snapshot create erweka-snapshot-"${{ inputs.repo-name }}" from repo erweka-"${{ inputs.repo-name }}"
+  aptly repo create -distribution=bionic -component=main erweka-"${REPO_NAME}"
+  aptly repo add erweka-"${REPO_NAME}" deb-package
+  aptly snapshot create erweka-snapshot-"${REPO_NAME}" from repo erweka-"${REPO_NAME}"
 
-  aptly snapshot merge -no-remove erweka-snapshot-"${{ inputs.repo-name }}"-merged-"${{ inputs.build-version }}" erweka-snapshot erweka-snapshot-"${{ inputs.repo-name }}"
-  aptly snapshot show -with-packages erweka-snapshot-"${{ inputs.repo-name }}"-merged-"${{ inputs.build-version }}"
-  aptly publish snapshot -force-overwrite -batch erweka-snapshot-"${{ inputs.repo-name }}"-merged-"${{ inputs.build-version }}" s3:erweka.repo:
+  aptly snapshot merge -no-remove erweka-snapshot-"${REPO_NAME}"-merged-"${BUILD_VERSION}" erweka-snapshot erweka-snapshot-"${REPO_NAME}"
+  aptly snapshot show -with-packages erweka-snapshot-"${REPO_NAME}"-merged-"${BUILD_VERSION}"
+  aptly publish snapshot -force-overwrite -batch erweka-snapshot-"${REPO_NAME}"-merged-"${BUILD_VERSION}" s3:erweka.repo:
 else
   aptly repo create -distribution=bionic -component=main erweka-deb
   aptly repo add erweka-deb deb-package
